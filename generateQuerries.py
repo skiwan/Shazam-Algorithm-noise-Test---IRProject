07ab7b66a9061dba3e6e6340f96a6f38cb34f30c
skiwan <merlinsewina@web.de>
2019-04-21 19:21:15 +0200

    Added all standard queries for 5s 10s 15s (half of all songs queried)

--- generateQuerries.py
+++ generateQuerries.py
@@ -2,32 +2,38 @@ import subprocess
 import os
 import random
 files = os.listdir("temp/")
+print('Toalfiles: ' + str(len(files)))
 fileLengths = []
-querryLen = int(input("How long (in seconds) shall the result querry be?"))
+querryLen = [5,10,15]
+files = random.sample(files,len(files)//2)
+print('Selected Files: ' + str(len(files)))
 
-# Create subfolder if not existend
-if not os.path.exists('querries/'+str(querryLen)):
-    os.makedirs('querries/'+str(querryLen))
+for ql in querryLen:
+    print("Strated with QL: " + str(ql))
+    # Create subfolder if not existend
+    if not os.path.exists('querries/'+str(ql)):
+        os.makedirs('querries/'+str(ql))
+    c = 0
+    # for every file in temp
+    for f in files:
+        # prepare output name
+        outf = 'querries/'+str(ql)+'/'+str(ql)+'query_'+f
 
-# for every file in temp
-for f in files:
-    # prepare output name
-    outf = 'querries/'+str(querryLen)+'query_'+f
+        # get duration inseconds of current song
+        command = ['ffprobe' ,'-v' ,'quiet' ,'-print_format' ,'compact=print_section=0:nokey=1:escape=csv' ,'-show_entries' ,'format=duration', 'temp/'+f]
+        ffmpeg_P = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out =(ffmpeg_P.communicate()[0])
+        out = out.replace("\r\n","")
+        duration = int(float(out))
+        if(duration > 50):
+            c+=1
+            # get random start time for querry
+            start = random.randint(20,(duration-duration//3))
+            end = start + ql
+            
+            command = ['ffmpeg','-i','temp/'+f, '-ss', str(start), '-t', str(end), outf]
+            ffmpeg_P = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # get duration inseconds of current song
-    command = ['ffprobe' ,'-v' ,'quiet' ,'-print_format' ,'compact=print_section=0:nokey=1:escape=csv' ,'-show_entries' ,'format=duration', 'temp/'+f]
-    ffmpeg_P = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out =(ffmpeg_P.communicate()[0])
-    out = out.replace("\r\n","")
-    print(out)
-    duration = int(float(out))
-
-    # get random start time for querry
-    start = random.randint(5,duration-10)
-    end = start + querryLen
-    
-    command = ['ffmpeg','-i','temp/'+f, '-ss', str(start), '-t', str(end), outf]
-    ffmpeg_P = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    output = ffmpeg_P.communicate()
+            output = ffmpeg_P.communicate()
+        print(c)
 
