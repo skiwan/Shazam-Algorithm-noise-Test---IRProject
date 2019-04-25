348a7732960281c1113b4a87fcceaaa457371a44
skiwan <merlinsewina@web.de>
2019-04-25 20:16:16 +0200

    added match gnereator for speech sample

---
+++ matchqspeech10_10.sh
@@ -0,0 +1,3 @@
+for filename in querries/speech10/10/*; do 
+	python audfprint/audfprint.py match --dbase audfprint/fulldbase.pklz "$filename"	
+done
---
+++ matchqspeech15_-10.sh
@@ -0,0 +1,3 @@
+for filename in querries/speech15/-10/*; do 
+	python audfprint/audfprint.py match --dbase audfprint/fulldbase.pklz "$filename"	
+done
